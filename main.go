1f6eecbfdf4819d24e481ef710c5c25f8512ff7d
Zachary Adam Kaplan <razic@viralkitty.com>
2014-12-03 21:08:23 +0000

    :lipstick:

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 func main() {
 	httpPort := os.Getenv("GLADIUS_HTTP_PORT")
 
-	log.Printf("Starting Gladius")
-	server.RegisterHandlers()
-	http.ListenAndServe(fmt.Sprintf(":%s", httpPort), nil)
 	log.Printf("Listening on port %s", httpPort)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", httpPort), nil)
 }
