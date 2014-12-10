e49242f6691da8e6987f2de87438b1c58b4f1009
Zachary Adam Kaplan <razic@viralkitty.com>
2014-12-10 10:06:02 +0000

    cleanup

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker build -t typekit/gladius . && docker run --rm --tty --interactive --volume $(which docker):$(which docker) --volume /var/run/docker.sock:/var/run/docker.sock --publish 8080:8080 typekit/gladius
+docker build -t typekit/gladius . && \
+	docker run \
+	--rm \
+	--tty \
+	--interactive \
+	--volume $(which docker):$(which docker) \
+	--volume /var/run/docker.sock:/var/run/docker.sock \
+	--publish 8080:8080 \
+	typekit/gladius "$@"
