6aa6da61a6ad6978cbaca552ff89649832011d35
Zachary Adam Kaplan <razic@viralkitty.com>
2015-01-17 02:28:10 +0000

    connect wth redis in routes

--- routes.go
+++ routes.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"code.google.com/p/go-uuid/uuid"
 	"encoding/json"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Routes struct {
@@ -12,11 +16,39 @@ type Routes struct {
 }
 
 func (r *Routes) Builds(w http.ResponseWriter, req *http.Request) {
+	var body []byte
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 
 	switch req.Method {
+	case "GET":
+		log.Printf("GET /builds")
+
+		keys, err := redis.Strings(redisCli.Do("KEYS", "pugio:builds:typekit:*"))
+		keysWithoutPrefix := make([]string, len(keys))
+
+		if err != nil {
+			log.Printf("Could not get keys: %s", err)
+			return
+		}
+
+		for i, key := range keys {
+			keysWithoutPrefix[i] = strings.Replace(key, "pugio:builds:typekit:", "", 1)
+		}
+
+		body, err = json.Marshal(keysWithoutPrefix)
+
+		if err != nil {
+			log.Printf("Could not marshal object")
+			return
+		}
+
+		w.Write(body)
 	case "POST":
+		log.Printf("POST /builds")
+
 		var b Build
 
 		body, err := ioutil.ReadAll(req.Body)
@@ -31,7 +63,16 @@ func (r *Routes) Builds(w http.ResponseWriter, req *http.Request) {
 			log.Fatal("Could not unmarshal the request body: ", err)
 		}
 
+		b.Id = uuid.New()
+
+		body, err = json.Marshal(b)
+
+		log.Printf("%+v", b)
+
 		go b.Create(r.Scheduler)
+		go redisCli.Do("HMSET", fmt.Sprintf("pugio:builds:typekit:%s", b.Id), "app", b.App, "branch", b.Branch)
+
+		w.Write(body)
 	default:
 	}
 }
