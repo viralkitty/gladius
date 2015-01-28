bae7bd9caa4ad07f4da7f53b51d46ccd19d1597f
Zachary Adam Kaplan <razic@viralkitty.com>
2015-01-28 01:30:34 +0000

    add tag to push options

--- build.go
+++ build.go
@@ -283,6 +283,7 @@ func (b *Build) pushImage() error {
 	pushOpts := docker.PushImageOptions{
 		Name:         b.FullImgName(),
 		OutputStream: &buf,
+		Tag:          b.Id(),
 	}
 
 	authConfig := docker.AuthConfiguration{}
